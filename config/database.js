cb61b173ebc3a088eb39a20c1c2383b4f563272f
JB Hutch <jbhutch01@gmail.com>
2022-10-29 10:44:41 -0400

    Update configs

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host || "localhost",
+      port: config.port || 5432,
+      database: config.database || "strapi",
+      user: config.user || "strapi",
+      password: config.password || "strapi",
+      ssl: false,
+    },
+    debug: false,
+  },
+});
